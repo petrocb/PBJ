1b678ef14370494e928320270753a69d069f5df7
petrocb <petrocbrown@gmail.com>
2023-06-21 00:31:56 +0100

    fixed

--- backtrader/main.py
+++ backtrader/main.py
@@ -3,22 +3,24 @@ import backtrader as bt
 
 class Strategy(bt.Strategy):
     def __init__(self):
-        self.diff = 0.0001
+        self.diff = 0.01
         self.buyOpen = False
         self.sellOpen = False
-        self.spread = 0
+        self.spread = 0.00011
 
     def next(self):
 
         self.price = self.data.close[0]  # Assuming you're using the close price as data
-        self.newAsk = self.price - self.diff
-        self.newBid = self.price + self.diff
+        self.bid = self.price - self.spread
+        self.ask = self.price + self.spread
+        self.newAsk = self.ask - self.diff
+        self.newBid = self.bid + self.diff
 
         if not self.buyOpen and not self.sellOpen:
-            self.startingAsk = self.price
-            self.startingBid = self.price
-            self.trailingBuy = self.price - self.diff
-            self.trailingSell = self.price + self.diff
+            self.startingAsk = self.ask
+            self.startingBid = self.bid
+            self.trailingBuy = self.ask - self.diff
+            self.trailingSell = self.bid + self.diff
             self.buyOpen = True
             self.sellOpen = True
 
@@ -33,47 +35,51 @@ class Strategy(bt.Strategy):
             # print(self.data.close[0])
             self.sell()
             self.buyOpen = False
-            print('OPEN!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+            print(self.data.datetime.date().isoformat(), 'SELL!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
                   'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
                   'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
+            print(cerebro.broker.getvalue())
 
         if self.trailingSell < self.price and self.buyOpen and self.sellOpen:
             # Open buy order
             # print(self.data.close[0])
             self.buy()
             self.sellOpen = False
-            print('OPEN!!!!!!!!!!''price:',self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+            print(self.data.datetime.date().isoformat(), 'BUY!!!!!!!!!!''price:',self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
                   'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
                   'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
+            print(cerebro.broker.getvalue())
 
         if self.trailingBuy > self.price and self.buyOpen:
             # Close buy order
             # print(self.data.close[0])
             self.close()
             self.buyOpen = False
-            print('CLOSE!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+            print(self.data.datetime.date().isoformat(), 'CLOSE!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
                   'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
                   'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
+            print(cerebro.broker.getvalue())
 
         if self.trailingSell < self.price and self.sellOpen:
             # Close sell order
             # print(self.data.close[0])
             self.close()
             self.sellOpen = False
-            print('CLOSE!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+            print(self.data.datetime.date().isoformat(), 'CLOSE!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
                   'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
                   'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
+            print(cerebro.broker.getvalue())
 
-        print('price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+        '''print('price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
               'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
-              'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
+              'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)'''
 
 
 cerebro = bt.Cerebro()
 
 cerebro.addstrategy(Strategy)
 
-data = bt.feeds.MT4CSVData(dataname='C:/Users/St Potrock/Desktop/fx.csv',
+data = bt.feeds.MT4CSVData(dataname='C:/Users/St Potrock/Desktop/EURUSD4.csv',
                            timeframe=bt.TimeFrame.Minutes,
                            compression=1)
 cerebro.adddata(data)
