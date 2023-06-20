409bcca6dbf9ce7522ce7c818c647d1c1c3730c5
petrocb <petrocbrown@gmail.com>
2023-06-20 14:27:31 +0100

    renamed trailing variables

--- backtrader/main.py
+++ backtrader/main.py
@@ -3,21 +3,22 @@ import backtrader as bt
 
 class Strategy(bt.Strategy):
     def __init__(self):
-        self.diff = 0.1
+        self.diff = 0.0001
         self.buyOpen = False
         self.sellOpen = False
+        self.spread = 0
 
     def next(self):
 
         self.price = self.data.close[0]  # Assuming you're using the close price as data
-        self.newAsk = self.data.close[0] + self.diff
-        self.newBid = self.data.close[0] - self.diff
+        self.newAsk = self.price - self.diff
+        self.newBid = self.price + self.diff
 
         if not self.buyOpen and not self.sellOpen:
-            self.startingAsk = self.data.close[0]
-            self.startingBid = self.data.close[0]
-            self.trailingBuy = self.data.close[0] + self.diff
-            self.trailingSell = self.data.close[0] - self.diff
+            self.startingAsk = self.price
+            self.startingBid = self.price
+            self.trailingBuy = self.price - self.diff
+            self.trailingSell = self.price + self.diff
             self.buyOpen = True
             self.sellOpen = True
 
@@ -27,43 +28,43 @@ class Strategy(bt.Strategy):
         if self.trailingSell > self.newBid:
             self.trailingSell = self.newBid
 
-        if self.trailingBuy > self.data.close[0] and self.buyOpen and self.sellOpen:
+        if self.trailingBuy > self.price and self.buyOpen and self.sellOpen:
             # Open sell order
             # print(self.data.close[0])
             self.sell()
             self.buyOpen = False
-            print('OPEN!!!!!!!!!!''price:', self.data.close[0], 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+            print('OPEN!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
                   'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
                   'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
 
-        if self.trailingSell < self.data.close[0] and self.buyOpen and self.sellOpen:
+        if self.trailingSell < self.price and self.buyOpen and self.sellOpen:
             # Open buy order
             # print(self.data.close[0])
             self.buy()
             self.sellOpen = False
-            print('OPEN!!!!!!!!!!''price:', self.data.close[0], 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+            print('OPEN!!!!!!!!!!''price:',self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
                   'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
                   'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
 
-        if self.trailingBuy > self.data.close[0] and self.buyOpen:
+        if self.trailingBuy > self.price and self.buyOpen:
             # Close buy order
             # print(self.data.close[0])
-            # self.close()
+            self.close()
             self.buyOpen = False
-            print('CLOSE!!!!!!!!!!''price:', self.data.close[0], 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+            print('CLOSE!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
                   'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
                   'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
 
-        if self.trailingSell < self.data.close[0] and self.sellOpen:
+        if self.trailingSell < self.price and self.sellOpen:
             # Close sell order
             # print(self.data.close[0])
-            # self.close()
+            self.close()
             self.sellOpen = False
-            print('CLOSE!!!!!!!!!!''price:', self.data.close[0], 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+            print('CLOSE!!!!!!!!!!''price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
                   'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
                   'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
 
-        print('price:', self.data.close[0], 'newAsk:', self.newAsk, 'newBid:', self.newBid,
+        print('price:', self.price, 'newAsk:', self.newAsk, 'newBid:', self.newBid,
               'startingAsk:', self.startingAsk, 'startingBid:', self.startingBid, 'trailingBuy:', self.trailingBuy,
               'tralingBid', self.trailingSell, 'buyOpen:', self.buyOpen, 'sellOpen:', self.sellOpen)
 
