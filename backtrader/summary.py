c254338b4c667a03e9d7b6da9193ca39f090eb4a
petrocb <petrocbrown@gmail.com>
2023-07-09 21:31:52 +0100

    added some functions

--- backtrader/summary.py
+++ backtrader/summary.py
@@ -1,2 +1,23 @@
 def summary ():
     return ("Hello World")
+
+def numObs(): #????????????????????
+    pass
+
+def numTrades():
+    pass
+
+def PnL():
+    pass
+
+def winRatio():
+    pass
+
+def aveHoldingWindow():
+    pass
+
+def maxHoldingWindow():
+    pass
+
+def minHoldingWindow():
+    pass
