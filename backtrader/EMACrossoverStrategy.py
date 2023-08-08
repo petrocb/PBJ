72b38ce933e1a4c1b2623fd654fcfc93820637cd
petrocb <petrocbrown@gmail.com>
2023-08-08 22:44:35 +0100

    fixed running and trading

--- backtrader/EMACrossoverStrategy.py
+++ backtrader/EMACrossoverStrategy.py
@@ -17,7 +17,7 @@ class EMACrossoverStrategy(bt.Strategy):
             self.buyOpen = False
             self.arr.append(['s', self.data.close[0], 0,  self.broker.getvalue(), self.data.datetime.datetime()])
             # Close Sell order: short EMA crosses above long EMA
-            if self.short_ema[0] > self.long_ema[0] and self.short_ema[-1] <= self.long_ema[-1]:
+        if self.short_ema[0] > self.long_ema[0] and self.short_ema[-1] <= self.long_ema[-1]:
                 self.close()
                 self.sellOpen = False
-                self.arr.append(['b', self.data.close[0], self.broker.getvalue(), self.data.datetime.datetime()])
+                self.arr.append(['cs', self.data.close[0], 0, self.broker.getvalue(), self.data.datetime.datetime()])
