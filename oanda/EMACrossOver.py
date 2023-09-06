87b43856086ae2f992affb1883f8dd1f232b9d8a
jackjones <dittotrading1603@gmail.com>
2023-09-06 23:41:26 +0100

    See what you think

--- oanda/EMACrossOver.py
+++ oanda/EMACrossOver.py
@@ -2,19 +2,21 @@ import requests
 import json
 from btalib import ema
 import pandas as pd
+import functions
 
 class EMACrossOver():
-    def __init__(self, list):
+    def __init__(self, closes):
         self.buyOpen = False
         self.sellOpen = False
         self.shortEma = self.emaCalc(10, list)
         self.longEma = self.emaCalc(30, list)
+        self.closes = functions.updatePastPrices()
 
     def tick(self):
         # if self.short_ema[0] < self.long_ema[0] and self.short_ema[-1] >= self.long_ema[-1]:
         pass
 
-    def emaCalc(self, period, list):
+    def emaCalc(self, period, closes):
         # print(type(list))
         # list = pd.DataFrame(list['candles'])
         # print(type(list))
@@ -22,7 +24,8 @@ class EMACrossOver():
         # return ema(list, period=period)
         #return 0
         # Extract the 'c' (close) values from the DataFrame
-        closes = [float(candle['mid']['c']) for candle in list['candles']]
-        closes = pd.Series(closes, index=pd.to_datetime([candle['time'] for candle in list['candles']]))
+        #closes = [float(candle['mid']['c']) for candle in list['candles']]
+        #closes = pd.Series(closes, index=pd.to_datetime([candle['time'] for candle in list['candles']]))
+
         # Calculate the EMA
         return ema(closes, period=period)
--- oanda/functions.py
+++ oanda/functions.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import datetime
+
+
 def getCred():
     return ["https://api-fxpractice.oanda.com", "554a05a67a483b45171693a0ded86b01-7f36009c47bba3095ed7d8cc9901486c",
             "101-004-25985927-001"]
@@ -25,18 +27,41 @@ def getAsk():
 
 
 def startPastPricesList():
-    time = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
+    time = datetime.datetime.utcnow() - datetime.timedelta(minutes=5)
     prices = requests.get(f"{getCred()[0]}/v3/accounts/{getCred()[2]}/instruments/EUR_USD/candles",
                           headers={'Authorization': f'Bearer {getCred()[1]}'},
-                          params={'granularity': 'M5'})
+                          params={'granularity': 'M5', 'count': 30})
     prices = prices.json()
-    return prices
+    close_prices = [candle['mid']['c'] for candle in prices['candles']]
+    return close_prices
 
-def updatePastPrices(list):
-    time = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
-    prices = requests.get(f"{getCred()[0]}/v3/accounts/{getCred()[2]}/instruments/EUR_USD/candles",
-                          headers={'Authorization': f'Bearer {getCred()[1]}'},
-                          params={'granularity': 'M5', 'from': list['candles'][-1]['time'] })
-    print(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.000Z'))
-    prices = prices.json()
-    return list.append(prices)
+
+def updatePastPrices(prices_list):
+    time = datetime.datetime.utcnow() - datetime.timedelta(minutes=5)
+    latest_prices = requests.get(f"{getCred()[0]}/v3/accounts/{getCred()[2]}/instruments/EUR_USD/candles",
+                                 headers={'Authorization': f'Bearer {getCred()[1]}'},
+                                 params={'granularity': 'M5', 'count': 30})
+
+    latest_prices = latest_prices.json()
+    latest_candles = latest_prices['candles']
+    latest_close_prices = [candle['mid']['c'] for candle in latest_candles]
+
+    # Remove excess data to keep only the last 30 close prices
+    if len(prices_list) + len(latest_close_prices) > 30:
+        num_to_remove = len(prices_list) + len(latest_close_prices) - 30
+        prices_list = prices_list[num_to_remove:]
+
+    # Extend the prices_list with the new close prices
+    prices_list.extend(latest_close_prices)
+    return prices_list
+
+
+# Example usage:
+past_close_prices = startPastPricesList()
+print("Initial Past Close Prices:")
+print(past_close_prices)
+
+# Simulate updating past close prices
+past_close_prices = updatePastPrices(past_close_prices)
+print("\nUpdated Past Close Prices (Last 30 Close Prices):")
+print(past_close_prices)
