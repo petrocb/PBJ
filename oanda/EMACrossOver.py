c6a307bcdb6c73b0c91e4ea76492c10105546c6e
jackjones <dittotrading1603@gmail.com>
2023-09-08 02:04:31 +0100

    Let me know what you think

--- oanda/EMACrossOver.py
+++ oanda/EMACrossOver.py
@@ -6,26 +6,51 @@ import functions
 
 class EMACrossOver():
     def __init__(self, closes):
-        self.buyOpen = False
-        self.sellOpen = False
-        self.shortEma = self.emaCalc(10, list)
-        self.longEma = self.emaCalc(30, list)
-        self.closes = functions.updatePastPrices()
+        self.buyOpen = False #Unsure if these need to be adjusted?
+        self.sellOpen = False #Unsure if these need to be adjusted?
+        self.shortEma = self.emaCalc(10, closes)  # Calculate short EMA with provided closes
+        self.longEma = self.emaCalc(30, closes)   # Calculate long EMA with provided closes
+        self.closes = closes
+        self.diff = 0.040
+        Entry_price = self.data.close
+        self.stop_loss = Entry_price + self.diff
 
     def tick(self):
-        # if self.short_ema[0] < self.long_ema[0] and self.short_ema[-1] >= self.long_ema[-1]:
-        pass
+        if self.shortEma[0] < self.longEma[0] and self.shortEma[-1] >= self.longEma[-1]:
+            # Sell signal: short EMA crosses below long EMA
+            Entry_price = self.closes
+            self.stop_loss = Entry_price + self.diff
+            self.sell()
+            self.buyOpen = False
+
+        if self.sellOpen == True:
+            if self.closes[-1] + self.diff < self.stop_loss[-1]:
+                self.stop_loss[0] = self.data.close[-1] + self.diff
+            elif self.closes[-1] + self.diff >= self.stop_loss[-1]:
+                self.stop_loss[0] = self.stop_loss[-1]
+            # Close Sell order: short EMA crosses above long EMA
+        if self.closes[0] >= self.stop_loss:
+            # self.short_ema[0] > self.long_ema[0] and self.short_ema[-1] <= self.long_ema[-1] or
+            self.close()
+            self.sellOpen = False
 
     def emaCalc(self, period, closes):
-        # print(type(list))
-        # list = pd.DataFrame(list['candles'])
-        # print(type(list))
-        #print(list)
-        # return ema(list, period=period)
-        #return 0
-        # Extract the 'c' (close) values from the DataFrame
-        #closes = [float(candle['mid']['c']) for candle in list['candles']]
-        #closes = pd.Series(closes, index=pd.to_datetime([candle['time'] for candle in list['candles']]))
-
-        # Calculate the EMA
         return ema(closes, period=period)
+
+    def buy(self):
+        # Place a buy order
+        # You want this to be done a certain way and im not sure of the code
+        # Need to define how to tell API to initiate trade in the way you want
+        pass
+
+    def sell(self):
+        # Place a sell order
+        # You want this to be done a certain way and im not sure of the code
+        # Need to define how to tell API to initiate trade in the way you want
+        pass
+
+    def close(self):
+        # Close an existing order
+        # You want this to be done a certain way and im not sure of the code
+        # Need to define how to tell API to initiate trade in the way you want
+        pass
