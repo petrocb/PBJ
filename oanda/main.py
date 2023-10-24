ba7cc266553007734a449c40b6c87a4d5f0731a0
petrocb <petrocbrown@gmail.com>
2023-10-24 19:47:07 +0100

    thing everyhting done

--- oanda/main.py
+++ oanda/main.py
@@ -8,12 +8,13 @@ import time
 import time
 from scraper import scaper
 def main():
-    x = NewSMACross([30, 10, 0.0005, 0.0006])
-    y = scaper()
-    while True:
-        x.tick()
-        y.tick()
-        time.sleep(60)
+    # x = NewSMACross([30, 10, 0.0005, 0.0006])
+    # y = scaper()
+    # while True:
+    #     x.tick()
+    #     y.tick()
+    #     time.sleep(60)
+    print( 6 < functions.time() < 19)
 
 if __name__ == "__main__":
     main()
