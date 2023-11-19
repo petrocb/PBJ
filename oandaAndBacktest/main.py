9d413c9190a05abbcdaa0ed829fcfdc3a1d02eef
jackjones <dittotrading1603@gmail.com>
2023-11-19 15:14:48 +0000

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	newBackTester/functions.py
    #	newBackTester/main.py

--- oandaAndBacktest/main.py
+++ oandaAndBacktest/main.py
@@ -1,41 +1,40 @@
 import functions
 from SMAFollowTrend import SMAFollowTrend
-from colored import Fore
+# from colored import Fore
 import datetime
 import time
 from summary import summary
+from SimpleSMA import SIMPLESMA
 def main():
-    # # a = followSMAangle()
-    # # b = FollowTrend()
+    # a = followSMAangle()
+    # b = FollowTrend()
     # c = SMAFollowTrend()
-    # # d = SMAFollowTrendStanDiv()
-    # # e = SMAFollowTrendStanDivCoolOff()
-    # # f = oncesMAFollowSD()
+    # d = SMAFollowTrendStanDiv()
+    # e = SMAFollowTrendStanDivCoolOff()
+    # f = oncesMAFollowSD()
     # x = functions.time() - datetime.timedelta(minutes=2)
     # while True:
     #     if datetime.datetime.utcnow() > x + datetime.timedelta(minutes=1):
     #         x = functions.time()
-    #         #         print(datetime.datetime.utcnow())
-    #         # print(f"{Fore.rgb('100%', '0%', '0%')}SMACross")
-    #         # a.tick()
-    #         # print(f"{Fore.rgb('100%', '100%', '0%')}FollowTrend")
-    #         # b.tick()
-    #         print(f"{Fore.rgb('100%', '0%', '100%')}SMAFollowTrend")
-    #         c.tick()
-    #         # print(f"{Fore.rgb('0%', '100%', '0%')}SMAFollowTrendSD")
-    #         # d.tick()
-    #         # print(f"{Fore.rgb('0%', '100%', '100%')}SMAFollowTrendSDCoolOff")
-    #         # e.tick()
-    #         # print(f"{Fore.rgb('0%', '0%', '100%')}onceFollow")
-    #         # f.tick()
+            #         print(datetime.datetime.utcnow())
+            # print(f"{Fore.rgb('100%', '0%', '0%')}SMACross")
+            # a.tick()
+            # print(f"{Fore.rgb('100%', '100%', '0%')}FollowTrend")
+            # b.tick()
+            # print(f"{Fore.rgb('100%', '0%', '100%')}SMAFollowTrend")
+            # c.tick()
+            # print(f"{Fore.rgb('0%', '100%', '0%')}SMAFollowTrendSD")
+            # d.tick()
+            # print(f"{Fore.rgb('0%', '100%', '100%')}SMAFollowTrendSDCoolOff")
+            # e.tick()
+            # print(f"{Fore.rgb('0%', '0%', '100%')}onceFollow")
+            # f.tick()
     #     time.sleep(10)
-    a = SMAFollowTrend()
+    a = SimpleSMA()
     try:
         while True:
             a.tick()
     except IndexError as e:
         summary([])
-
-
 if __name__ == "__main__":
     main()
